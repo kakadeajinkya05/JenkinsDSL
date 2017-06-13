5ff2d4e3f008deee9f6a54fcb31bd650af462cf2
Ajinkya Kakade <kakadeajinkya05@gmail.com>
2017-06-13 22:53:56 +0530

    Update GroovyDSL.groovy

--- src/main/groovy/com/jenkins/GroovyDSL.groovy
+++ src/main/groovy/com/jenkins/GroovyDSL.groovy
@@ -9,7 +9,7 @@ job('Master Build and Test') {
                 wipeOutWorkspace()
             }
             branch '*/MD_MASTER*'
-	    credentialsId('bbid')
+	    credentials('bbid')
         }
     }
 
