07a3ba0aa73b4e6ddee201824f27e4f3fca5ebb7
Ajinkya Kakade <kakadeajinkya05@gmail.com>
2017-06-15 17:35:36 +0530

    Added radiator view

---
+++ src/main/groovy/com/jenkins/Radiator.groovy
@@ -0,0 +1,8 @@
+buildMonitorView('Radiator') {
+    description('Radiator for all Services')
+    jobs {
+        name('Recrument Portal Build and Test')
+        name('SeedJob')
+        
+    }
+}
