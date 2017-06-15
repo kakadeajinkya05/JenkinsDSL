2b28f435bf330277bfb0ee2eed90192a1e25eb7b
Ajinkya Kakade <kakadeajinkya05@gmail.com>
2017-06-15 18:10:37 +0530

    Update Radiator.groovy

--- src/main/groovy/com/jenkins/Radiator.groovy
+++ src/main/groovy/com/jenkins/Radiator.groovy
@@ -1,7 +1,7 @@
 buildMonitorView('Radiator View') {
     description('Radiator for all Services')
     jobs {
-        name('Recrument Portal Build and Test')
+        name('Recruitment Portal Build and Test')
         name('SeedJob')
         
     }
